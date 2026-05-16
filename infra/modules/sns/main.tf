b6e9336be14e879b12006afbf3b262fe74607047
Willianf-Silva <willian.ferreira.da.silva@gmail.com>
2026-05-16 18:20:46 -0300

    feat: adiciona modulo sns com subscription para sqs

---
+++ infra/modules/sns/main.tf
@@ -0,0 +1,9 @@
+resource "aws_sns_topic" "events" {
+  name = "${var.app_name}-events"
+}
+
+resource "aws_sns_topic_subscription" "sqs" {
+  topic_arn = aws_sns_topic.events.arn
+  protocol  = "sqs"
+  endpoint  = var.sqs_queue_arn
+}
